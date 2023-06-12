1d9ea3a72d7a3d73bfe445a4ebb0a86e1263a94c
IanAguilera24 <120074568+IanAguilera24@users.noreply.github.com>
2023-06-12 00:56:29 -0600

    Delete src directory

--- src/main/java/com/escom/evento/repo/IAsistenteRepo.java
+++
@@ -1,9 +0,0 @@
-package com.escom.evento.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.escom.evento.model.Asistente;
-
-public interface IAsistenteRepo extends JpaRepository<Asistente, Long> {
-
-}
